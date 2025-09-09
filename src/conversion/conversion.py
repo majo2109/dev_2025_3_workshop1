29c533b8ddd2ebce780e07c77b94ba0b091fc7b0
majo2109 <majorincon2109@gmail.com>
2025-09-09 12:11:10 -0500

    2

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -54,7 +54,8 @@ class Conversion:
 
     def romano_a_decimal(self, romano):
         """
-        Convierte un número romano a decimal.
+        Convierte un número romano a decimal
+        .
         """
         valores = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
         total = 0
